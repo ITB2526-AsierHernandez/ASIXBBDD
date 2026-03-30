3a3ce63dee9dcd45fdee56b76dfa30643533b7d7
ITB2526-AsierHernandez <asier.hernandez.7e7@itb.cat>
2026-03-30 11:51:18 +0000

    hasta la consulta 34

--- consulta.sql
+++ consulta.sql
@@ -9,3 +9,15 @@ SELECT count(nom) AS persones FROM dades_ponent WHERE nom = "anna" AND cognom = 
 SELECT nom FROM dades_ponent WHERE nom = "anna" OR cognom = "rovira" GROUP BY nom;
 SELECT count(*) AS cuenta FROM dades_ponent WHERE nom = "anna" OR cognom = "rovira" GROUP BY nom;
 SELECT count(*) AS cuenta FROM dades_ponent WHERE nom = "anna" OR cognom = "rovira" ORDER BY count(*);
+SELECT CONCAT(nom,' ',cognom) AS "Nombre y apellido" FROM dades_ponent;
+SELECT SUBSTRING(nom,1,3) AS "3 primeras letras mas fecha", CURRENT_DATE AS "FECHA" FROM dades_ponent;
+SELECT CURRENT_DATE() FROM DUAL;
+SELECT 18+3 FROM DUAL;
+ALTER TABLE dades_ponent ADD level INT;
+alter table dades_ponent ADD COLUMN ordinadors int check (ordinadors >= 0 AND ordinadors <= 4);
+alter table dades_ponent ADD COLUMN rate int check (rate BETWEEN 5 AND 10);
+UPDATE dades_ponent
+SET 
+    ordinadors = FLOOR(1 + RAND() * 4),   -- valores entre 1 y 4
+    rate = FLOOR(5 + RAND() * 6);
+SELECT * FROM sessio;
